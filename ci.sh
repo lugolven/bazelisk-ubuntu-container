bc640810270ff250af9bb70dee7b30ff6797cb8d
Lucas Golven <lucas@golven.fr>
2026-06-04 22:44:19 -0700

    missing-files

--- ci.sh
+++ ci.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
-echo "Building the image..." >&2
+echo "::group::Building the tag image..." >&2
 TAG_IMAGE_ID=$(docker build . --target tag --iidfile /tmp/image-id && cat /tmp/image-id)
+echo "::endgroup::" >&2
 
-echo "Generating the tag for the image..." >&2
+echo "::group::Generating the tag for the image..." >&2
 TAG=$(docker run --rm $TAG_IMAGE_ID)
+echo "::endgroup::" >&2
 
 echo "Publishing the image with tag $TAG" >&2
