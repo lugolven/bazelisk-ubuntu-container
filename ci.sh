878222ead99523d5a6358f5544516963410cac3a
Lucas Golven <lucas@golven.fr>
2026-06-04 22:45:36 -0700

    log folding

--- ci.sh
+++ ci.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-echo "::group::Building the tag image..." >&2
+echo "::group::Building the tag image..."
 TAG_IMAGE_ID=$(docker build . --target tag --iidfile /tmp/image-id && cat /tmp/image-id)
-echo "::endgroup::" >&2
+echo "::endgroup::"
 
-echo "::group::Generating the tag for the image..." >&2
+echo "::group::Generating the tag for the image..."
 TAG=$(docker run --rm $TAG_IMAGE_ID)
-echo "::endgroup::" >&2
+echo "::endgroup::"
 
-echo "Publishing the image with tag $TAG" >&2
+echo "Publishing the image with tag $TAG"
